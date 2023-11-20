3e6703b8431ba31948a3066d638287a2fceb1261
Victor070510 <vreis1585@gmail.com>
2023-11-20 19:11:13 -0300

    teste de javascript

--- script.js
+++ script.js
@@ -140,3 +140,33 @@ document.addEventListener('DOMContentLoaded', function() {
     calcularTotal();
   });
 });
+
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const address = document.getElementById('address').value;
+  const phone = document.getElementById('phone').value;
+  const pickup = document.getElementById('pickup').value;
+  const payment = document.getElementById('payment').value;
+
+  document.getElementById('confirmName').textContent = name;
+  document.getElementById('confirmAddress').textContent = address;
+  document.getElementById('confirmPhone').textContent = phone;
+  document.getElementById('confirmPickup').textContent = pickup;
+  document.getElementById('confirmPayment').textContent = payment;
+
+  // Adicionando os dados do formulário ao objeto do pedido
+  const pedido = {
+      nome: name,
+      endereco: address,
+      telefone: phone,
+      retirada: pickup,
+      pagamento: payment
+  };
+
+  // Exibindo os dados do pedido nos detalhes
+  document.getElementById('confirmation').classList.remove('hidden');
+  // Chame a função para atualizar os detalhes do pedido com os dados do objeto 'pedido'
+});
