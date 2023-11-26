7634aa888585ae0f610c3fe545ae062d6eae35f6
Victor070510 <vreis1585@gmail.com>
2023-11-25 22:46:21 -0300

    teste de back end

--- app.py
+++ app.py
@@ -1,51 +1,45 @@
 from flask import Flask, request, jsonify
-import sqlite3
+import mysql.connector
 
 app = Flask(__name__)
 
-# Configuração do banco de dados
-DATABASE = 'database.db'
-
-def create_table():
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS pedidos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT,
-            endereco TEXT,
-            telefone TEXT,
-            retirada TEXT,
-            pagamento TEXT,
-            total REAL
-        )
-    ''')
-    conn.commit()
-    conn.close()
+# Tente estabelecer uma conexão
+try:
+    mydb = mysql.connector.connect(
+        host="localhost:3306",
+        user="root",
+        password="victor0705",
+        database="seu_banco_de_dados"
+    )
+    print("Conexão com o banco de dados MySQL estabelecida com sucesso!")
+except mysql.connector.Error as err:
+    print(f"Erro ao conectar ao banco de dados: {err}")
 
 @app.route('/sua-rota', methods=['POST'])
 def receber_pedido():
     data = request.json
 
-    # Aqui você pode acessar os dados do pedido e salvá-los no banco de dados
-    nome = data.get('name')
-    endereco = data.get('address')
-    telefone = data.get('phone')
-    retirada = data.get('pickup')
-    pagamento = data.get('payment')
-    total = data.get('total')
+    cursor = mydb.cursor()
 
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute('''
-        INSERT INTO pedidos (nome, endereco, telefone, retirada, pagamento, total)
-        VALUES (?, ?, ?, ?, ?, ?)
-    ''', (nome, endereco, telefone, retirada, pagamento, total))
-    conn.commit()
-    conn.close()
+    # Inserir detalhes do pedido na tabela 'pedidos'
+    pedido_sql = "INSERT INTO pedidos (nome, endereco, telefone, responsavel_retirada, forma_pagamento, total) VALUES (%s, %s, %s, %s, %s, %s)"
+    pedido_values = (data['name'], data['address'], data['phone'], data['pickup'], data['payment'], data['total'])
+    cursor.execute(pedido_sql, pedido_values)
+    mydb.commit()
 
-    return jsonify({'message': 'Pedido recebido e salvo no banco de dados'})
+    # Obter o ID do pedido inserido
+    pedido_id = cursor.lastrowid
+
+    # Inserir itens do carrinho na tabela 'itens_carrinho'
+    for item in data['cartItems']:
+        item_sql = "INSERT INTO itens_carrinho (id_pedido, nome_produto, quantidade, preco_unitario, total_item) VALUES (%s, %s, %s, %s, %s)"
+        item_values = (pedido_id, item['name'], item['quantity'], item['price'], item['quantity'] * item['price'])
+        cursor.execute(item_sql, item_values)
+        mydb.commit()
+
+    cursor.close()
+
+    return jsonify({"message": "Pedido recebido e inserido no banco de dados"}), 200
 
 if __name__ == '__main__':
-    create_table()
     app.run(debug=True)
