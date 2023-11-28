e6cbcc1414ac35745f1a09359ba73ae1292a68b5
Victor070510 <vreis1585@gmail.com>
2023-11-27 23:12:56 -0300

    teste de backend

--- app.py
+++ app.py
@@ -1,64 +1,85 @@
-from flask import Flask, request, jsonify, render_template
-import mysql.connector
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-def conectar_banco():
-    db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="victor0705",
-        database="nando_pasteis"
+# Configurações do banco de dados
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'victor0705'
+app.config['MYSQL_DB'] = 'nando_pasteis'
+
+mysql = MySQL(app)
+
+# Rota para criar um pedido
+@app.route('/pedido', methods=['POST'])
+def criar_pedido():
+    cur = mysql.connection.cursor()
+    nome = request.json['nome']
+    endereco = request.json['endereco']
+    telefone = request.json['telefone']
+    responsavel_retirada = request.json['responsavel_retirada']
+    forma_pagamento = request.json['forma_pagamento']
+
+    cur.execute(
+        "INSERT INTO Pedidos (nome, endereco, telefone, responsavel_retirada, forma_pagamento) VALUES (%s, %s, %s, %s, %s)",
+        (nome, endereco, telefone, responsavel_retirada, forma_pagamento)
     )
-    return db
 
-@app.route('/pedido', methods=['GET', 'POST']) 
-def receber_pedido():
-    if request.method == 'POST':
-        try:
-            db = conectar_banco()
-            cur = db.cursor()
+    mysql.connection.commit()
+    cur.close()
 
-            data = request.json
+    return jsonify({'message': 'Pedido criado com sucesso!'})
 
-            nome = data['name']
-            endereco = data['address']
-            telefone = data['phone']
-            responsavel_retirada = data['pickup']
-            forma_pagamento = data['payment']
-            total = data['total']
+# Rota para obter todos os pedidos
+@app.route('/pedidos', methods=['GET'])
+def obter_pedidos():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM Pedidos")
+    pedidos = cur.fetchall()
+    cur.close()
 
-            # Inserir informações na tabela pedidos
-            insert_pedido = "INSERT INTO pedidos (nome, endereco, telefone, responsavel_retirada, forma_pagamento, total) VALUES (%s, %s, %s, %s, %s, %s)"
-            pedido_values = (nome, endereco, telefone, responsavel_retirada, forma_pagamento, total)
-            cur.execute(insert_pedido, pedido_values)
-            db.commit()
+    return jsonify({'pedidos': pedidos})
 
-            pedido_id = cur.lastrowid
+# Rota para obter um pedido por ID
+@app.route('/pedido/<int:pedido_id>', methods=['GET'])
+def obter_pedido(pedido_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM Pedidos WHERE id = %s", (pedido_id,))
+    pedido = cur.fetchone()
+    cur.close()
 
-            # Inserir informações na tabela itens_carrinho
-            for item in data['cartItems']:
-                nome_produto = item['name']
-                quantidade = item['quantity']
-                preco_unitario = item['price']
-                total_item = quantidade * preco_unitario
+    return jsonify({'pedido': pedido})
 
-                insert_item_carrinho = "INSERT INTO itens_carrinho (id_pedido, nome_produto, quantidade, preco_unitario, total_item) VALUES (%s, %s, %s, %s, %s)"
-                item_values = (pedido_id, nome_produto, quantidade, preco_unitario, total_item)
-                cur.execute(insert_item_carrinho, item_values)
-                db.commit()
+# Rota para atualizar um pedido por ID
+@app.route('/pedido/<int:pedido_id>', methods=['PUT'])
+def atualizar_pedido(pedido_id):
+    cur = mysql.connection.cursor()
+    nome = request.json['nome']
+    endereco = request.json['endereco']
+    telefone = request.json['telefone']
+    responsavel_retirada = request.json['responsavel_retirada']
+    forma_pagamento = request.json['forma_pagamento']
 
-            cur.close()
-            db.close()
+    cur.execute(
+        "UPDATE Pedidos SET nome=%s, endereco=%s, telefone=%s, responsavel_retirada=%s, forma_pagamento=%s WHERE id=%s",
+        (nome, endereco, telefone, responsavel_retirada, forma_pagamento, pedido_id)
+    )
 
-            return render_template('pedido.html', message='Pedido recebido e armazenado com sucesso!')
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
+    mysql.connection.commit()
+    cur.close()
 
-    elif request.method == 'GET':
-        render_template('index.html', message='teste') 
+    return jsonify({'message': 'Pedido atualizado com sucesso!'})
 
-    return jsonify({'message': 'Método não permitido'}), 405
+# Rota para deletar um pedido por ID
+@app.route('/pedido/<int:pedido_id>', methods=['DELETE'])
+def deletar_pedido(pedido_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM Pedidos WHERE id = %s", (pedido_id,))
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify({'message': 'Pedido deletado com sucesso!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
